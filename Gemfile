14539bb36eaf374569d8a7fdfabcf7ed84b36ea6
Andrej Tazky <ayo@ynet.sk>
2011-11-10 14:38:38 +0100

    Added user following

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'gravatar_image_tag', '1.0.0.pre2'
 
 gem 'will_paginate', '3.0.pre2'
 
+gem 'faker', '0.3.1'
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
@@ -35,8 +37,7 @@ gem 'will_paginate', '3.0.pre2'
 
 group :development do
   gem 'rspec-rails', '2.6.1'
-  gem 'annotate', '2.4.0' 
-  gem 'faker', '0.3.1'	
+  gem 'annotate', '2.4.0' 	
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,20 @@ SampleApp::Application.routes.draw do
  
   get "sessions/new"
 
+
   resources :sessions, :only => [:new, :create, :destroy]
 
   resources :users
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :microposts, :only => [:create, :destroy]
+
+  resources :relationships, :only => [:create, :destroy]
 
   match '/signup',  :to => 'users#new'
 
